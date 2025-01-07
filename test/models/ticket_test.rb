54b09b27ae17fec4bca8250f67d775c543d8fd91
Ali Sürücü <progmas@gmail.com>
2025-01-08 01:02:26 +0300

    test methods have been added.

--- test/models/ticket_test.rb
+++ test/models/ticket_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 class TicketTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:one)
+    @event = events(:one)
+    @ticket = Ticket.new(event: @event, user: @user, quantity: 1)
+  end
+
+  test "should be valid" do
+    assert @ticket.valid?
+  end
+
+  test "quantity should be positive" do
+    @ticket.quantity = 0
+    assert_not @ticket.valid?
+  end
+
+  test "should belong to an event" do
+    assert_equal @event, @ticket.event
+  end
+
+  test "should belong to a user" do
+    assert_equal @user, @ticket.user
+  end
 end
